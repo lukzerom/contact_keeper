79270c7783bb8e690478d6ac553d4a90703c9af6
Lukasz <lukzerom@gmail.com>
2020-02-16 16:49:38 +0100

    Finished functionality of UI, now integrating frontend and backend, and authorisation

---
+++ client/src/components/contacts/Contacts.jsx
@@ -0,0 +1,34 @@
+import React, { Fragment, useContext } from 'react';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
+import ContactContext from '../../context/contact/contactContext';
+import ContactItem from './ContactItem';
+
+const Contacts = () => {
+  const contactContext = useContext(ContactContext);
+
+  const { contacts, filtered } = contactContext;
+
+  if (contacts.length === 0) {
+    return <h4>Please add contact</h4>;
+  }
+
+  return (
+    <Fragment>
+      <TransitionGroup>
+        {filtered !== null
+          ? filtered.map(contact => (
+              <CSSTransition key={contact.id} timeout={50} classNames='item'>
+                <ContactItem key={contact.id} contact={contact} />
+              </CSSTransition>
+            ))
+          : contacts.map(contact => (
+              <CSSTransition key={contact.id} timeout={50} classNames='item'>
+                <ContactItem key={contact.id} contact={contact} />
+              </CSSTransition>
+            ))}
+      </TransitionGroup>
+    </Fragment>
+  );
+};
+
+export default Contacts;
